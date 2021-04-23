c1635330e4bcb171476fe899681fa36b2b92f354
lidya-toscano <75463460+lidya-toscano@users.noreply.github.com>
2021-04-22 19:25:15 -0700

    added new folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather />
+      <footer>
+        <h5>
+          <a
+            href="https://github.com/lidya-toscano/react-weather-app"
+            target="nonreferrer"
+          >
+            Open-Source Code
+          </a>{" "}
+          by{" "}
+          <a href="https://www.lidyatoscano.com/" target="_blank">
+            Lidya Toscano
+          </a>
+        </h5>
+      </footer>
     </div>
   );
 }
-
-export default App;
